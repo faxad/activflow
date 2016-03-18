fb9911348cd53c03e9e36b2a8c95efd5f8d770ab
Fawad Qureshi <fawadhq@gmail.com>
2016-03-18 23:12:15 +0300

    added tests for request initiation

--- djangoflow/tests/tests.py
+++ djangoflow/tests/tests.py
@@ -1,5 +1,5 @@
 """Tests for Core app"""
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User, Group
 from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.test import TestCase
@@ -9,136 +9,181 @@ from djangoflow.tests.models import Foo
 
 
 class CoreTests(TestCase):
-    """Core CRUD tests"""
+    """Core workflow engine tests"""
     def setUp(self, **kwargs):
         """Test Setup"""
         self.client = Client()
 
-        user = User.objects.create_user(
+        self.submitter = Group.objects.create(name='Submitter')
+        self.reviewer = Group.objects.create(name='Reviewer')
+
+        self.john_doe = User.objects.create_user(
             'john_doe',
             'john@company.com',
             '12345')
 
-        self.foo = Foo.objects.create(
-            bar='Sample', baz='CR', qux='Not Available')
-
-        content_type = ContentType.objects.get_for_model(Foo)
-
-        permissions = [Permission.objects.get_or_create(
-            codename='{}_foo'.format(permission),
-            content_type=content_type)[0] for permission in (
-                'view', 'delete', 'change', 'add')]
-
-        user.user_permissions.set(permissions)
+        self.jane_smith = User.objects.create_user(
+            'jane_smith',
+            'jane@company.com',
+            '12345')
 
         self.client.login(username='john_doe', password='12345')
 
-    def test_index(self):
-        """Test main view that lists all models"""
-        response = self.client.get(reverse('main'))
+    def test_available_workflows(self):
+        """Tests view that lists workflows"""
+        response = self.client.get(reverse('workflows'))
         self.assertEqual(response.status_code, 200)
 
-    def test_detail_view(self):
-        """Tests for detail view"""
+    def test_workflow_detail_view_with_no_requests(self):
+        """Test to get requests for a workflow"""
         response = self.client.get(reverse(
-            'detail',
-            kwargs={
-                'app_name': 'tests',
-                'model_name': 'Foo',
-                'pk': self.foo.id
-            }))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'CR')
-
-    def test_list_view(self):
-        """Tests for list view"""
-        response = self.client.get(reverse(
-            'index',
-            kwargs={'app_name': 'tests', 'model_name': 'Foo'}))
+            'workflow-detail',
+            kwargs={'app_name': 'tests'}))
 
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(
-            response.context['objects'],
-            [repr(self.foo)])
+            response.context['instances'], [])
 
-    def test_delete_view(self):
-        """Tests for delete view"""
-        response = self.client.post(reverse(
-            'delete',
-            kwargs={
-                'app_name': 'tests',
-                'model_name': 'Foo',
-                'pk': self.foo.id
-            }))
+    def test_initiate_request(self):
+        """Tests request initiation"""
+        request_args = {
+            'app_name': 'tests',
+            'model_name': 'Foo',
+            'pk': 'Initial'
+        }
 
-        self.assertRedirects(
-            response,
-            reverse(
-                'index',
-                kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
-            status_code=302,
-            target_status_code=200)
-
-        count = Foo.objects.filter(id=self.foo.id).count()
-
-        self.assertEqual(count, 0)
-
-    def test_create_view(self):
-        """Tests for create view"""
         for verb in ('get', 'post'):
             response = getattr(self.client, verb)(reverse(
                 'create',
-                kwargs={'app_name': 'tests', 'model_name': 'Foo'}))
+                kwargs=request_args))
+
+            if verb == 'get':
+                self.assertTrue('Permission Denied' in response.content)
+                self.submitter.user_set.add(self.john_doe)
+
+            response = getattr(self.client, verb)(reverse(
+                'create',
+                kwargs=request_args))
 
             self.assertEqual(response.context['form']._meta.model, Foo)
 
         response = self.client.post(reverse(
             'create',
-            kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
+            kwargs=request_args),
             {'bar': 'example - small e', 'baz': 'WL', 'qux': 'Nothing'})
 
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(Foo.objects.count(), 0)
 
         response = self.client.post(reverse(
             'create',
-            kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
+            kwargs=request_args),
             {'bar': 'Example - big E', 'baz': 'WL', 'qux': 'Nothing'})
+
+        instance = Foo.objects.all()
+        self.assertEqual(instance.count(), 1)
 
         self.assertRedirects(
             response,
             reverse(
-                'index',
-                kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
-            status_code=302,
-            target_status_code=200)
-
-    def test_udpate_view(self):
-        """Tests for update view"""
-        for verb in ('get', 'post'):
-            response = getattr(self.client, verb)(reverse(
                 'update',
                 kwargs={
                     'app_name': 'tests',
                     'model_name': 'Foo',
-                    'pk': self.foo.id
-                }))
-
-            self.assertEqual(response.context['form']._meta.model, Foo)
-
-        response = self.client.post(reverse(
-            'update',
-            kwargs={
-                'app_name': 'tests',
-                'model_name': 'Foo',
-                'pk': self.foo.id
-            }),
-            {'bar': 'Example', 'baz': 'WL', 'qux': 'Nothing'})
-
-        self.assertRedirects(
-            response,
-            reverse(
-                'index',
-                kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
+                    'pk': instance.first().id
+                }),
             status_code=302,
             target_status_code=200)
+
+
+    # def test_list_view(self):
+    #     """Tests for list view"""
+    #     response = self.client.get(reverse(
+    #         'index',
+    #         kwargs={'app_name': 'tests', 'model_name': 'Foo'}))
+
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertQuerysetEqual(
+    #         response.context['objects'],
+    #         [repr(self.foo)])
+
+    # def test_delete_view(self):
+    #     """Tests for delete view"""
+    #     response = self.client.post(reverse(
+    #         'delete',
+    #         kwargs={
+    #             'app_name': 'tests',
+    #             'model_name': 'Foo',
+    #             'pk': self.foo.id
+    #         }))
+
+    #     self.assertRedirects(
+    #         response,
+    #         reverse(
+    #             'index',
+    #             kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
+    #         status_code=302,
+    #         target_status_code=200)
+
+    #     count = Foo.objects.filter(id=self.foo.id).count()
+
+    #     self.assertEqual(count, 0)
+
+    # def test_create_view(self):
+    #     """Tests for create view"""
+    #     for verb in ('get', 'post'):
+    #         response = getattr(self.client, verb)(reverse(
+    #             'create',
+    #             kwargs={'app_name': 'tests', 'model_name': 'Foo'}))
+
+    #         self.assertEqual(response.context['form']._meta.model, Foo)
+
+    #     response = self.client.post(reverse(
+    #         'create',
+    #         kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
+    #         {'bar': 'example - small e', 'baz': 'WL', 'qux': 'Nothing'})
+
+    #     self.assertEqual(response.status_code, 200)
+
+    #     response = self.client.post(reverse(
+    #         'create',
+    #         kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
+    #         {'bar': 'Example - big E', 'baz': 'WL', 'qux': 'Nothing'})
+
+    #     self.assertRedirects(
+    #         response,
+    #         reverse(
+    #             'index',
+    #             kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
+    #         status_code=302,
+    #         target_status_code=200)
+
+    # def test_udpate_view(self):
+    #     """Tests for update view"""
+    #     for verb in ('get', 'post'):
+    #         response = getattr(self.client, verb)(reverse(
+    #             'update',
+    #             kwargs={
+    #                 'app_name': 'tests',
+    #                 'model_name': 'Foo',
+    #                 'pk': self.foo.id
+    #             }))
+
+    #         self.assertEqual(response.context['form']._meta.model, Foo)
+
+    #     response = self.client.post(reverse(
+    #         'update',
+    #         kwargs={
+    #             'app_name': 'tests',
+    #             'model_name': 'Foo',
+    #             'pk': self.foo.id
+    #         }),
+    #         {'bar': 'Example', 'baz': 'WL', 'qux': 'Nothing'})
+
+    #     self.assertRedirects(
+    #         response,
+    #         reverse(
+    #             'index',
+    #             kwargs={'app_name': 'tests', 'model_name': 'Foo'}),
+    #         status_code=302,
+    #         target_status_code=200)
